bc7af6b566cef4c07d63c266d2e7605287745c39
Konrad Gądek <kgadek@gmail.com>
2015-05-26 01:54:19 +0200

    Blocks are red

--- page/goL.js
+++ page/goL.js
@@ -1,6 +1,7 @@
 var goL = (function(){
   var goL ={},bits=128*256;
   var dt; // grid bits
+  var dtB; // grid bits [block]
   var i;  // which dt is current?
   var sp=0; // speed of animation. smaller = faster.
   var timeClr ;// for clearing Timeout.
@@ -8,7 +9,9 @@ var goL = (function(){
   
   goL.clearGrid = function(){
     dt = [  Array.apply(0, Array(bits/32)).map(function() { return 0; }),
-        Array.apply(0, Array(bits/32)).map(function() { return 0; })]; //enough bits for 64x128 grid
+        Array.apply(0, Array(bits/32)).map(function() { return 0; })];
+    dtB = [  Array.apply(0, Array(bits/32)).map(function() { return 0; }),
+        Array.apply(0, Array(bits/32)).map(function() { return 0; })];
     i =0;// 0 is current
   }
   
@@ -19,6 +22,14 @@ var goL = (function(){
   goL.put = function(p,s,j){ // set status of pth bit to s
     s ? (dt[j][p>>>5]|=(0x80000000>>>(p&31))): (dt[j][p>>>5]&=~(0x80000000>>>(p&31)));
   }
+
+  goL.getB = function(p){ // get status of pth bit
+    return (dtB[i][p>>>5]&(0x80000000>>>(p&31))) ==0?0:1;
+  }
+  
+  goL.putB = function(p,s,j){ // set status of pth bit to s
+    s ? (dtB[j][p>>>5]|=(0x80000000>>>(p&31))): (dtB[j][p>>>5]&=~(0x80000000>>>(p&31)));
+  }
   
   goL.clearTimeout = function(){
     clearTimeout(timeClr);
@@ -28,11 +39,17 @@ var goL = (function(){
     editMode=f;
     f? rects.attr('class',"edit") : rects.attr('class',"");
   }
+
+  goL.blockColour = function(d){
+    bird = goL.get(d);
+    block = goL.getB(d);
+    return block == 1 ? "red" : (bird == 1 ? "steelblue" : "white");
+  }
   
   function switchSq(p){
     if(!editMode) return;
     goL.put(p,1-goL.get(p),i);
-    rects.style("fill",function(d){ return goL.get(d)==1? "steelblue":"white";});
+    rects.style("fill",function(d){ return goL.blockColour(d); });
   }
   
   goL.clearGrid();
@@ -40,7 +57,7 @@ var goL = (function(){
       .attr("x",function(d){ return (d&255)*5;})
       .attr("y",function(d){ return (d>>>8)*5;})
       .attr("width",5).attr("height",5)
-      .style("fill",function(d){ return goL.get(d)==1? "steelblue":"white";})
+      .style("fill",function(d){ return goL.blockColour(d); })
       .on("click",function(d){ return switchSq(d);});
   
   goL.setGrid = function(ar){  //set the initial bits
@@ -51,7 +68,7 @@ var goL = (function(){
       d3.range(0,ar.length).forEach(function(y){d3.range(0,ar[0].length)
         .forEach(function(x){goL.put(y*256+x,ar[y][x],i)})});
     } 
-    rects.style("fill",function(d){ return goL.get(d)==1? "steelblue":"white";});
+    rects.style("fill",function(d){ return goL.blockColour(d); });
   }
   
   goL.update = function(){
@@ -64,7 +81,13 @@ var goL = (function(){
 
         for(var p=0; p<bits; p++){
           if(resjson[resptr]['xy'] === p){
-            goL.put(p, 1, 1-i);
+            disp = resjson[resptr]['disp'];
+            if(disp===1){
+              goL.put(p, 1, 1-i);    
+            } else if(disp===2) {
+              goL.putB(p, 1, 1-i);    
+            }
+            
             if(resptr+1 < resjson.length){
                 resptr = resptr + 1;
             }
@@ -76,7 +99,7 @@ var goL = (function(){
     });
 
     i=1-i;// switch current
-    rects.style("fill",function(d){ return goL.get(d)==1? "steelblue":"white";});
+    rects.style("fill",function(d){ return goL.blockColour(d); });
     
     timeClr = setTimeout(function(){goL.update()},sp);
   }
